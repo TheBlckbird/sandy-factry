6790bd427f6a04f3e46c8435ae7c434104e6f179
Louis Weigel <louis.weigel@tls-giessen.eu>
2025-04-23 20:36:26 +0200

    Do some restructuring of place buildings

---
+++ src/plugins/building/place_buildings.rs
@@ -0,0 +1,213 @@
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+use bevy_ecs_tilemap::prelude::*;
+
+use super::{
+    BuildEvent, BuildingComponent, BuildingInput, BuildingOutput, CurrentBuilding, Foreground,
+    ForegroundObject, HoverBuilding,
+};
+
+pub fn place_buildings(
+    mut commands: Commands,
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    window: Query<&Window, With<PrimaryWindow>>,
+    buttons: Res<ButtonInput<MouseButton>>,
+    mut tilemap_q: Query<
+        (
+            Entity,
+            &TilemapSize,
+            &TilemapGridSize,
+            &TilemapType,
+            &Transform,
+            &mut TileStorage,
+        ),
+        With<Foreground>,
+    >,
+    tile_query: Query<
+        (Entity, &TilePos, Option<&HoverBuilding>, &TileTextureIndex),
+        With<Foreground>,
+    >,
+    current_building: Res<CurrentBuilding>,
+    mut event_writer: EventWriter<BuildEvent>,
+) {
+    let (camera, camera_transform) = camera_query.single();
+    let window = window.single();
+
+    let cursor_position = match window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world_2d(camera_transform, cursor).ok())
+    {
+        Some(position) => position,
+        None => return,
+    };
+
+    let (tilemap_entity, map_size, grid_size, map_type, map_transform, mut tile_storage) =
+        tilemap_q.single_mut();
+
+    let cursor_in_map_position = {
+        let cursor_position = Vec4::from((cursor_position, 0.0, 1.0));
+        let cursor_in_map_position = map_transform.compute_matrix().inverse() * cursor_position;
+        cursor_in_map_position.xy()
+    };
+
+    let mouse_tile_pos =
+        match TilePos::from_world_pos(&cursor_in_map_position, map_size, grid_size, map_type) {
+            Some(position) => position,
+            None => return,
+        };
+
+    let mut is_other_tile_at_mouse = false;
+
+    for (tile_entity, tile_pos, hover, _) in tile_query.iter() {
+        if *tile_pos == mouse_tile_pos && hover.is_none() {
+            is_other_tile_at_mouse = true;
+        } else if hover.is_some() {
+            commands.entity(tile_entity).despawn_recursive();
+            tile_storage.remove(tile_pos);
+        }
+    }
+
+    if buttons.pressed(MouseButton::Right) {
+        // erasing mode
+
+        for (tile_entity, tile_pos, hover, texture_index) in tile_query.iter() {
+            if *tile_pos == mouse_tile_pos || hover.is_some() {
+                commands.entity(tile_entity).despawn_recursive();
+                tile_storage.remove(&mouse_tile_pos);
+
+                event_writer.send(BuildEvent::Deleted(
+                    *tile_pos,
+                    ForegroundObject::from_tile_texture_index(texture_index),
+                ));
+            }
+        }
+
+        return;
+    }
+
+    let foreground_object: ForegroundObject = current_building.into_foreground_object();
+
+    let Some(tile_texture_index) = foreground_object.into_tile_texture_index() else {
+        return;
+    };
+
+    if is_other_tile_at_mouse {
+        return;
+    }
+
+    if buttons.pressed(MouseButton::Left) {
+        // building mode
+
+        let new_tile_entity = commands
+            .spawn((
+                TileBundle {
+                    position: mouse_tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    texture_index: tile_texture_index,
+                    ..Default::default()
+                },
+                Foreground,
+                BuildingComponent::new(
+                    Vec::new(),
+                    match foreground_object.into_building_type() {
+                        Some(building_type) => building_type,
+                        None => return,
+                    },
+                ),
+                BuildingInput(foreground_object.get_input_side()),
+                BuildingOutput(foreground_object.get_output_side()),
+            ))
+            .id();
+
+        event_writer.send(BuildEvent::Placed(
+            mouse_tile_pos,
+            ForegroundObject::from_tile_texture_index(&tile_texture_index),
+        ));
+
+        tile_storage.set(&mouse_tile_pos, new_tile_entity);
+    } else {
+        // hover mode
+
+        let new_tile_entity = commands
+            .spawn((
+                TileBundle {
+                    position: mouse_tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    texture_index: tile_texture_index,
+                    color: TileColor(Color::srgba(1.0, 1.0, 1.0, 0.7)),
+                    ..Default::default()
+                },
+                Foreground,
+                HoverBuilding,
+            ))
+            .id();
+
+        tile_storage.set(&mouse_tile_pos, new_tile_entity);
+    }
+}
+
+// Belt direction
+
+// let neighbors = Neighbors::get_square_neighboring_positions(&mouse_tile_pos, &map_size, false);
+// let north_neighbor = neighbors.north;
+// let east_neighbor = neighbors.east;
+// let south_neighbor = neighbors.south;
+// let west_neighbor = neighbors.west;
+
+// let mut connection_options: Vec<(Direction, Direction)> = Vec::new();
+
+// let find_foreground_object = |searched_tile_pos| {
+//     let mut foreground_object = None;
+
+//     for (_, tile_pos, _, tile_texture_index) in tile_query.iter() {
+//         if *tile_pos == searched_tile_pos {
+//             foreground_object = Some(ForegroundObject::from_tile_texture_index(
+//                 tile_texture_index,
+//             ));
+//         }
+//     }
+
+//     foreground_object.unwrap()
+// };
+
+// let foreground_input_direction = foreground_object.get_input_side();
+
+// let mut north_neighbor_output_direction = None;
+// let mut east_neighbor_output_direction = None;
+// let mut south_neighbor_output_direction = None;
+// let mut west_neighbor_output_direction = None;
+
+// if let Some(north_neighbor) = north_neighbor {
+//     let north_foreground_object = find_foreground_object(north_neighbor);
+//     north_neighbor_output_direction = north_foreground_object.get_output_side();
+// }
+
+// if let Some(east_neighbor) = east_neighbor {
+//     let east_foreground_object = find_foreground_object(east_neighbor);
+//     east_neighbor_output_direction = east_foreground_object.get_output_side();
+// }
+
+// if let Some(south_neighbor) = south_neighbor {
+//     let south_foreground_object = find_foreground_object(south_neighbor);
+//     south_neighbor_output_direction = south_foreground_object.get_output_side();
+// }
+
+// if let Some(west_neighbor) = west_neighbor {
+//     let west_foreground_object = find_foreground_object(west_neighbor);
+//     west_neighbor_output_direction = west_foreground_object.get_output_side();
+// }
+
+// if let Some(north_neighbor_output_direction) = north_neighbor_output_direction {
+//     if let Some(foreground_input_direction) = foreground_input_direction {
+//         if north_neighbor_output_direction
+//         match foreground_input_direction {
+//             Direction::North | Direction::South => {}
+//             Direction::East => todo!(),
+//             Direction::West => todo!(),
+//         }
+//     }
+// }
+
+// todo!();
+
+// Belt direction end
