TheBlckbird/sandy-factry

commit ffd7ffa538e9321f1092258b5cebe489e3db9400

Fix being able to place multiple tiles in the same position (#43)
Closes #42

src/plugins/building/mod.rs
@@ -445,6 +445,21 @@ fn place_buildings(
         return;
     };
 
+    let mut is_other_tile_at_mouse = false;
+
+    for (tile_entity, tile_pos, hover, _) in tile_query.iter() {
+        if *tile_pos == mouse_tile_pos && hover.is_none() {
+            is_other_tile_at_mouse = true;
+        } else if hover.is_some() {
+            commands.entity(tile_entity).despawn_recursive();
+            tile_storage.remove(tile_pos);
+        }
+    }
+
+    if is_other_tile_at_mouse {
+        return;
+    }
+
     if buttons.pressed(MouseButton::Left) {
         // building mode
 
@@ -485,35 +500,22 @@ fn place_buildings(
     } else {
         // hover mode
 
-        let mut is_other_tile_at_mouse = false;
+        let new_tile_entity = commands
+            .spawn((
+                TileBundle {
+                    position: mouse_tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    texture_index: tile_texture_index,
+                    color: TileColor(Color::srgba(1.0, 1.0, 1.0, 0.7)),
+                    ..Default::default()
+                },
+                Foreground,
+                HoverBuilding,
+            ))
+            .id();
 
-        for (tile_entity, tile_pos, hover, _) in tile_query.iter() {
-            if *tile_pos == mouse_tile_pos && hover.is_none() {
-                is_other_tile_at_mouse = true;
-            } else if hover.is_some() {
-                commands.entity(tile_entity).despawn_recursive();
-                tile_storage.remove(tile_pos);
-            }
-        }
-
-        if !is_other_tile_at_mouse {
-            let new_tile_entity = commands
-                .spawn((
-                    TileBundle {
-                        position: mouse_tile_pos,
-                        tilemap_id: TilemapId(tilemap_entity),
-                        texture_index: tile_texture_index,
-                        color: TileColor(Color::srgba(1.0, 1.0, 1.0, 0.7)),
-                        ..Default::default()
-                    },
-                    Foreground,
-                    HoverBuilding,
-                ))
-                .id();
-
-            tile_storage.set(&mouse_tile_pos, new_tile_entity);
-        }
-    };
+        tile_storage.set(&mouse_tile_pos, new_tile_entity);
+    }
 }
 
 /*

commit be144e9f0734699b698c3f2ac224865c60115c19

Fix panic regarding hover tile deleting introduced in #43

src/plugins/building/mod.rs
@@ -435,13 +435,6 @@ fn place_buildings(
     let foreground_object: ForegroundObject = current_building.into_foreground_object();
 
     let Some(tile_texture_index) = foreground_object.into_tile_texture_index() else {
-        for (tile_entity, tile_pos, hover, _) in tile_query.iter() {
-            if hover.is_some() {
-                commands.entity(tile_entity).despawn_recursive();
-                tile_storage.remove(tile_pos);
-            }
-        }
-
         return;
     };
 
@@ -462,13 +455,6 @@ fn place_buildings(
 
     if buttons.pressed(MouseButton::Left) {
         // building mode
-
-        for (tile_entity, tile_pos, hover, _) in tile_query.iter() {
-            if hover.is_some() {
-                commands.entity(tile_entity).despawn_recursive();
-                tile_storage.remove(tile_pos);
-            }
-        }
 
         let new_tile_entity = commands
             .spawn((