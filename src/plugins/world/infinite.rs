932c62d75bff88ed3ae801c9d598c9dc67d81c0a
TheBlckbird <louis.weigel@tls-giessen.eu>
2025-05-01 19:43:46 +0200

    Add resource patches (#58)
    * Add resource tiles
    
    * Add generation of resource patches

--- src/plugins/world/infinite.rs
+++ src/plugins/world/infinite.rs
@@ -1,8 +1,10 @@
 use bevy::prelude::*;
 use bevy_ecs_tilemap::prelude::*;
+use rand::{Rng, rngs::StdRng};
 
 use super::{
-    Background, BackgroundObject, CHUNK_SIZE, Chunk, ChunkManager, RENDER_CHUNK_SIZE, TILE_SIZE,
+    Background, BackgroundObject, CHUNK_SIZE, Chunk, ChunkManager, GlobalRng, Middleground,
+    MiddlegroundObject, RENDER_CHUNK_SIZE, TILE_SIZE,
 };
 
 pub fn spawn_chunks_around_camera(
@@ -10,6 +12,7 @@ pub fn spawn_chunks_around_camera(
     asset_server: Res<AssetServer>,
     camera_query: Query<&Transform, With<Camera>>,
     mut chunk_manager: ResMut<ChunkManager>,
+    mut rng: ResMut<GlobalRng>,
 ) {
     for transform in camera_query.iter() {
         let camera_chunk_pos = camera_pos_to_chunk_pos(&transform.translation.xy());
@@ -22,7 +25,7 @@ pub fn spawn_chunks_around_camera(
             {
                 if !chunk_manager.spawned_chunks.contains(&IVec2::new(x, y)) {
                     chunk_manager.spawned_chunks.insert(IVec2::new(x, y));
-                    spawn_chunk(&mut commands, &asset_server, IVec2::new(x, y));
+                    spawn_chunk(&mut commands, &asset_server, IVec2::new(x, y), &mut rng.0);
                 }
             }
         }
@@ -50,11 +53,21 @@ pub fn despawn_outofrange_chunks(
     }
 }
 
-fn spawn_chunk(commands: &mut Commands, asset_server: &AssetServer, chunk_pos: IVec2) {
-    let tilemap_entity = commands.spawn_empty().id();
-    let mut tile_storage = TileStorage::empty(CHUNK_SIZE.into());
+fn spawn_chunk(
+    commands: &mut Commands,
+    asset_server: &AssetServer,
+    chunk_pos: IVec2,
+    rng: &mut StdRng,
+) {
+    let translation = Vec2::new(
+        chunk_pos.x as f32 * CHUNK_SIZE.x as f32 * TILE_SIZE.x,
+        chunk_pos.y as f32 * CHUNK_SIZE.y as f32 * TILE_SIZE.y,
+    );
 
-    // Spawn the elements of the tilemap.
+    let background_tilemap_entity = commands.spawn_empty().id();
+    let mut background_tile_storage = TileStorage::empty(CHUNK_SIZE.into());
+
+    // Spawn the tiles for the background tilemap
     for x in 0..CHUNK_SIZE.x {
         for y in 0..CHUNK_SIZE.y {
             let tile_pos = TilePos { x, y };
@@ -62,42 +75,99 @@ fn spawn_chunk(commands: &mut Commands, asset_server: &AssetServer, chunk_pos: I
                 .spawn((
                     TileBundle {
                         position: tile_pos,
-                        tilemap_id: TilemapId(tilemap_entity),
+                        tilemap_id: TilemapId(background_tilemap_entity),
                         texture_index: BackgroundObject::DefaultTile.into(),
                         ..Default::default()
                     },
                     Background,
                 ))
                 .id();
-            commands.entity(tilemap_entity).add_child(tile_entity);
-            tile_storage.set(&tile_pos, tile_entity);
+            commands
+                .entity(background_tilemap_entity)
+                .add_child(tile_entity);
+            background_tile_storage.set(&tile_pos, tile_entity);
         }
     }
 
-    let transform = Transform::from_translation(Vec3::new(
-        chunk_pos.x as f32 * CHUNK_SIZE.x as f32 * TILE_SIZE.x,
-        chunk_pos.y as f32 * CHUNK_SIZE.y as f32 * TILE_SIZE.y,
-        0.0,
-    ));
-    let texture_handle: Handle<Image> = asset_server.load("background_tiles.png");
+    let background_texture_handle: Handle<Image> = asset_server.load("background_tiles.png");
 
-    commands.entity(tilemap_entity).insert((
-        TilemapBundle {
-            grid_size: TILE_SIZE.into(),
-            size: CHUNK_SIZE.into(),
-            storage: tile_storage,
-            texture: TilemapTexture::Single(texture_handle),
-            tile_size: TILE_SIZE,
-            transform,
-            render_settings: TilemapRenderSettings {
-                render_chunk_size: RENDER_CHUNK_SIZE,
-                ..Default::default()
-            },
-            ..Default::default()
-        },
+    // Spawn the background tilemap
+    commands.entity(background_tilemap_entity).insert((
+        make_tilemap_bundle(
+            Transform::from_translation(translation.extend(0.0)),
+            background_texture_handle,
+            background_tile_storage,
+        ),
         Background,
         Chunk,
     ));
+
+    let middleground_tilemap_entity = commands.spawn_empty().id();
+    let mut middleground_tile_storage = TileStorage::empty(CHUNK_SIZE.into());
+
+    // Spawn the tiles for the middleground tilemap
+    let maybe_resource_type = match rng.random_range(0..20) {
+        0 => Some(MiddlegroundObject::Coal),
+        1 => Some(MiddlegroundObject::Iron),
+        2 => Some(MiddlegroundObject::Copper),
+        _ => None,
+    };
+
+    if let Some(resource_type) = maybe_resource_type {
+        for x in 0..CHUNK_SIZE.x {
+            for y in 0..CHUNK_SIZE.y {
+                let tile_pos = TilePos { x, y };
+                let tile_entity = commands
+                    .spawn((
+                        TileBundle {
+                            position: tile_pos,
+                            tilemap_id: TilemapId(middleground_tilemap_entity),
+                            texture_index: resource_type.into(),
+                            ..Default::default()
+                        },
+                        Middleground,
+                    ))
+                    .id();
+                commands
+                    .entity(middleground_tilemap_entity)
+                    .add_child(tile_entity);
+                middleground_tile_storage.set(&tile_pos, tile_entity);
+            }
+        }
+
+        let middleground_texture_handle = asset_server.load("middleground_tiles.png");
+
+        // Spawn the middleground tilemap
+        commands.entity(middleground_tilemap_entity).insert((
+            make_tilemap_bundle(
+                Transform::from_translation(translation.extend(1.0)),
+                middleground_texture_handle,
+                middleground_tile_storage,
+            ),
+            Middleground,
+            Chunk,
+        ));
+    }
+}
+
+fn make_tilemap_bundle(
+    transform: Transform,
+    texture_handle: Handle<Image>,
+    tile_storage: TileStorage,
+) -> TilemapBundle {
+    TilemapBundle {
+        grid_size: TILE_SIZE.into(),
+        size: CHUNK_SIZE.into(),
+        storage: tile_storage,
+        texture: TilemapTexture::Single(texture_handle),
+        tile_size: TILE_SIZE,
+        transform,
+        render_settings: TilemapRenderSettings {
+            render_chunk_size: RENDER_CHUNK_SIZE,
+            ..Default::default()
+        },
+        ..Default::default()
+    }
 }
 
 fn camera_pos_to_chunk_pos(camera_pos: &Vec2) -> IVec2 {
--- src/plugins/world/mod.rs
+++ src/plugins/world/mod.rs
@@ -1,6 +1,7 @@
 use bevy::{prelude::*, utils::HashSet};
 use bevy_ecs_tilemap::prelude::*;
 use infinite::{despawn_outofrange_chunks, spawn_chunks_around_camera};
+use rand::{SeedableRng, rngs::StdRng};
 
 mod infinite;
 
@@ -13,23 +14,30 @@ pub const RENDER_CHUNK_SIZE: UVec2 = UVec2 {
     y: CHUNK_SIZE.y * 2,
 };
 
+#[derive(Resource)]
+struct GlobalRng(pub StdRng);
+
+impl Default for GlobalRng {
+    fn default() -> Self {
+        Self(StdRng::from_seed(rand::random()))
+    }
+}
+
 #[derive(Component)]
 pub struct Background;
 
 #[derive(Component)]
+pub struct Middleground;
+
+#[derive(Component)]
 pub struct Chunk;
 
-#[derive(Resource, Default, Clone, Copy)]
+#[derive(Default, Clone, Copy)]
 enum BackgroundObject {
     Dirt,
     Water,
     #[default]
     DefaultTile,
-}
-
-#[derive(Default, Debug, Resource)]
-struct ChunkManager {
-    pub spawned_chunks: HashSet<IVec2>,
 }
 
 impl From<BackgroundObject> for TileTextureIndex {
@@ -44,13 +52,39 @@ impl From<BackgroundObject> for TileTextureIndex {
     }
 }
 
+#[derive(Clone, Copy)]
+enum MiddlegroundObject {
+    Coal,
+    Copper,
+    Iron,
+}
+
+impl From<MiddlegroundObject> for TileTextureIndex {
+    fn from(value: MiddlegroundObject) -> Self {
+        let index = match value {
+            MiddlegroundObject::Coal => 0,
+            MiddlegroundObject::Copper => 1,
+            MiddlegroundObject::Iron => 2,
+        };
+
+        TileTextureIndex(index)
+    }
+}
+
+#[derive(Default, Debug, Resource)]
+struct ChunkManager {
+    pub spawned_chunks: HashSet<IVec2>,
+}
+
 pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<ChunkManager>().add_systems(
-            Update,
-            (spawn_chunks_around_camera, despawn_outofrange_chunks),
-        );
+        app.init_resource::<ChunkManager>()
+            .init_resource::<GlobalRng>()
+            .add_systems(
+                Update,
+                (spawn_chunks_around_camera, despawn_outofrange_chunks),
+            );
     }
 }
