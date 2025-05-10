1d1e66a04d524f7b4a991055323a8593c6d4896c
Louis Weigel <louis.weigel@tls-giessen.eu>
2025-05-10 16:08:58 +0200

    Preserve building state when converting from world to graph and back

--- src/plugins/simulation/graph_to_world.rs
+++ src/plugins/simulation/graph_to_world.rs
@@ -9,19 +9,20 @@ pub fn graph_to_world(
     simulation_graph: Res<SimulationGraph>,
     mut tile_query: Query<(&TilePos, &mut Machine)>,
 ) {
-    for (building, building_tile_pos) in simulation_graph
+    for (machine, building_tile_pos) in simulation_graph
         .0
         .node_indices()
         .map(|node_index| &simulation_graph.0[node_index])
     {
-        let Some((_, mut building_component)) = tile_query
+        let Some((_, mut machine_component)) = tile_query
             .iter_mut()
             .find(|&(tile_pos, _)| tile_pos == building_tile_pos)
         else {
             return;
         };
 
-        building_component.input_items = building.input_items.clone();
-        building_component.output_items = building.output_items.clone();
+        machine_component.input_items = machine.input_items.clone();
+        machine_component.output_items = machine.output_items.clone();
+        machine_component.machine_type = machine.machine_type.clone_box();
     }
 }
