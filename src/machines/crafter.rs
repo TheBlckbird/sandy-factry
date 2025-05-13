a6fd7fccd417a123780c5aec1b877415d0e008c6
Louis Weigel <louis.weigel@tls-giessen.eu>
2025-05-13 11:16:26 +0200

    Add crafting time

--- src/machines/crafter.rs
+++ src/machines/crafter.rs
@@ -7,7 +7,11 @@ use super::{InputItems, InputSide, Item, MachineType, OutputItems};
 #[derive(Debug, Clone, Default)]
 pub struct Crafter {
     current_recipe: Option<CrafterRecipe>,
+    /// Burn time left
     burn_time: u8,
+    /// Crafting time left
+    /// `None` if nothing is currently being crafting
+    crafting_time_left: Option<u8>,
 }
 
 impl Crafter {
@@ -19,6 +23,7 @@ impl Crafter {
         Self {
             current_recipe: Some(current_recipe),
             burn_time: 0,
+            crafting_time_left: None,
         }
     }
 }
@@ -53,42 +58,64 @@ impl MachineType for Crafter {
             None => return,
         };
 
-        if self.burn_time >= Self::CRAFTING_BURN_TIME {
-            let mut items = HashMap::new();
-            let items_input = input_items
-                .north
-                .as_mut()
-                .expect("A Crafter should have a north input");
+        let mut unset_crafting_time = false;
 
-            // Convert the queue into a HashMap of all the items and their count
-            for item in items_input.iter() {
-                items
-                    .entry(*item)
-                    .and_modify(|count| *count += 1)
-                    .or_insert(1);
-            }
+        // Check whether something is currently being crafted
+        match self.crafting_time_left.as_mut() {
+            Some(crafting_time_left) => {
+                if *crafting_time_left == 0 {
+                    // Crafting finished
+                    // Append the crafted item to `output_items`
+                    for _ in 0..current_recipe.output_count {
+                        output_items.push_back(current_recipe.output_item);
+                    }
 
-            // Try the crafting recipe and return this function if there aren't enough items
-            // else assign the HashMap of the remaining items to `rest_items`
-            let rest_items = match current_recipe.try_crafting(&items) {
-                Some(rest_items) => rest_items,
-                None => return,
-            };
-
-            items_input.clear();
-            self.burn_time -= Self::CRAFTING_BURN_TIME;
-
-            // Transfer the `rest_items` back into `items_input`
-            for (item, count) in rest_items.into_iter() {
-                for _ in 0..count {
-                    items_input.push_back(item);
+                    unset_crafting_time = true;
+                } else {
+                    // Crafting not finished,
+                    *crafting_time_left -= 1;
                 }
             }
+            None => {
+                if self.burn_time >= Self::CRAFTING_BURN_TIME {
+                    let mut items = HashMap::new();
+                    let items_input = input_items
+                        .north
+                        .as_mut()
+                        .expect("A Crafter should have a north input");
 
-            // Append the crafted item to `output_items`
-            for _ in 0..current_recipe.output_count {
-                output_items.push_back(current_recipe.output_item);
+                    // Convert the queue into a HashMap of all the items and their count
+                    for item in items_input.iter() {
+                        items
+                            .entry(*item)
+                            .and_modify(|count| *count += 1)
+                            .or_insert(1);
+                    }
+
+                    // Try the crafting recipe and return this function if there aren't enough items
+                    // else assign the HashMap of the remaining items to `rest_items`
+                    let rest_items = match current_recipe.try_crafting(&items) {
+                        Some(rest_items) => rest_items,
+                        None => return,
+                    };
+
+                    items_input.clear();
+                    self.burn_time -= Self::CRAFTING_BURN_TIME;
+
+                    // Transfer the `rest_items` back into `items_input`
+                    for (item, count) in rest_items.into_iter() {
+                        for _ in 0..count {
+                            items_input.push_back(item);
+                        }
+                    }
+
+                    self.crafting_time_left = Some(current_recipe.crafting_time);
+                }
             }
+        }
+
+        if unset_crafting_time {
+            self.crafting_time_left = None;
         }
     }
 
