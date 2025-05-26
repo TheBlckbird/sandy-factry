b8d860d265246a73df4b0ca9d0e44589143a710f
Louis Weigel <louis.weigel@tls-giessen.eu>
2025-05-26 19:31:46 +0200

    Suppress warnings in crafting recipes

--- src/plugins/crafting/mod.rs
+++ src/plugins/crafting/mod.rs
@@ -1,58 +1,31 @@
+#![allow(unused)] // [TODO] Remove once this is all implemented
 use std::collections::HashMap;
 
 use bevy::prelude::*;
 use recipe_types::{CrafterRecipe, FurnaceRecipe};
 
-use crate::machines::Item;
-
 use super::menu::GameState;
 
 pub mod recipe_types;
 
-#[derive(Debug, Resource)]
+#[derive(Debug, Resource, Default)]
 pub struct CrafterRecipes(Vec<CrafterRecipe>);
 
-#[derive(Debug, Resource)]
+#[derive(Debug, Resource, Default)]
 pub struct FurnaceRecipes(Vec<FurnaceRecipe>);
 
 pub struct CraftingPlugin;
 
 impl Plugin for CraftingPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(
-            OnEnter(GameState::Game),
-            (startup, list_all_recipes).chain(),
-        )
-        .add_systems(OnExit(GameState::Game), cleanup);
+        app.add_systems(OnEnter(GameState::Game), startup)
+            .add_systems(OnExit(GameState::Game), cleanup);
     }
 }
 
 fn startup(mut commands: Commands) {
-    commands.insert_resource(CrafterRecipes(vec![
-        // [TODO] Remove test recipe
-        CrafterRecipe::new(
-            HashMap::from([(Item::Coal, 1), (Item::RawCopper, 2)]),
-            Item::CopperIngot,
-            1,
-            1,
-        ),
-    ]));
-
-    commands.insert_resource(FurnaceRecipes(vec![]));
-}
-
-// [DEBUG] Remove this
-fn list_all_recipes(crafter_recipes: Res<CrafterRecipes>) {
-    for recipe in crafter_recipes.0.iter() {
-        info!(
-            "The Recipe for {} {:?} uses the ingredients {:?} and takes {} second{}",
-            recipe.output_count,
-            recipe.output_item,
-            recipe.ingredients,
-            recipe.crafting_time,
-            if recipe.crafting_time == 1 { "" } else { "s" }
-        )
-    }
+    commands.init_resource::<FurnaceRecipes>();
+    commands.init_resource::<FurnaceRecipes>();
 }
 
 fn cleanup(mut commands: Commands) {
