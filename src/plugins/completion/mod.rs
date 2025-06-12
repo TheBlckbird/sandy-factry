c8c89177d39f8096bab1dfc26c877297c816430a
Louis Weigel <louis.weigel@tls-giessen.eu>
2025-06-12 12:51:21 +0200

    Add comments to completion plugin

--- src/plugins/completion/mod.rs
+++ src/plugins/completion/mod.rs
@@ -10,9 +10,7 @@ use crate::{
 
 mod check_completion;
 
-#[derive(Resource, Default, Deref, DerefMut)]
-pub struct HasCompletedGame(bool);
-
+// MARK: Plugin
 pub struct CompletionPlugin;
 
 impl Plugin for CompletionPlugin {
@@ -26,15 +24,26 @@ impl Plugin for CompletionPlugin {
     }
 }
 
+// MARK: Resources
+
+#[derive(Resource, Default, Deref, DerefMut)]
+pub struct HasCompletedGame(bool);
+
+// MARK: Systems
+
+/// Initialize Resources for [CompletionPlugin]
 fn setup(mut commands: Commands, game_save: Res<LoadedGameSave>) {
+    // Retrieve completed state or set it to false
     let has_completed = match &**game_save {
         Some(game_save) => game_save.has_completed_game,
         None => false,
     };
 
+    // Store completed state
     commands.insert_resource(HasCompletedGame(has_completed));
 }
 
+/// Remove the resources for [CompletionPlugin]
 fn cleanup(mut commands: Commands) {
     commands.remove_resource::<HasCompletedGame>();
 }
