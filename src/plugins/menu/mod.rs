331f29b06a08f095ee31b3137ed49c7fe6a0f03d
Louis Weigel <louis.weigel@tls-giessen.eu>
2025-06-23 10:45:43 +0200

    Fix color of pressed button

--- src/plugins/menu/mod.rs
+++ src/plugins/menu/mod.rs
@@ -15,8 +15,8 @@ mod splash_screen;
 
 const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
-const HOVERED_PRESSED_BUTTON: Color = Color::srgb(0.25, 0.65, 0.25);
-const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+const HOVERED_PRESSED_BUTTON: Color = Color::srgb(0.35, 0.35, 0.35);
+const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.35, 0.35);
 
 const MAIN_TEXT_COLOR: Color = Color::srgb(0.1, 0.1, 0.1);
 const TEXT_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
