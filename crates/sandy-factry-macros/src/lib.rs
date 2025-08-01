370f3d116cc153e23b683820e1719445d76209cf
Louis Weigel <louis.weigel@tls-giessen.eu>
2025-08-01 21:02:38 +0200

    Improve foreground objects macro error message for invalid input

--- crates/sandy-factry-macros/src/lib.rs
+++ crates/sandy-factry-macros/src/lib.rs
@@ -1,10 +1,11 @@
 use foreground_objects::impl_foreground_objects_macro;
 use proc_macro::TokenStream;
+use syn::{DeriveInput, parse_macro_input};
 
 mod foreground_objects;
 
 #[proc_macro_derive(ForegroundObjects, attributes(variant))]
 pub fn foreground_objects_derive(input: TokenStream) -> TokenStream {
-    let ast = syn::parse(input).expect("Invalid Rust Code");
+    let ast = parse_macro_input!(input as DeriveInput);
     impl_foreground_objects_macro(&ast)
 }
